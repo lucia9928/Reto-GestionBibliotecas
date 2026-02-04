7032b02d51d56fe1768d1dca639c115b659e3a3c
in_esp <inakidemiguel06@gmail.com>
2026-02-04 09:27:34 +0100

    revisar relacion

---
+++ models/autor.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from config.database import Base
+
+class Autor(Base):
+    __tablename__ = "autor"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    nationality = Column(String(50),nullable=False)
+    birth_year = Column(Integer, nullable=True)
+
+    # Relaciones
+    re_libros = relationship("Libros", back_populates="autor")
+
+
