456aefdd7032bdb0a2cb1ead3f8f5e718df8e1fb
lucia9928 <lucia2801salvas@gmail.com>
2026-02-04 11:55:27 +0100

    endpoint de autor

--- models/prestamo.py
+++ models/prestamo.py
@@ -8,7 +8,6 @@ class Prestamo(Base):
     __tablename__ = "prestamo"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    book_id = Column(Integer, ForeignKey=Libro.id)
     borrower_name = Column(String(100), nullable=False)
     borrower_email = Column(String(120), nullable=False)
     #FALTA VALIDAR FORMATO DE FECHA
--- routers/prestamos.py
+++ routers/prestamos.py
@@ -1,29 +1,32 @@
 from typing import List
 
-from fastapi import HTTPException
+from fastapi import HTTPException,APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from config.database import get_db
 from schemas.prestamo import PrestamoResponse
 from models.prestamo import Prestamo
-from main import  app
+router = APIRouter(
+prefix="/prestamos",
+tags=["prestamos"],
+)
 
 
-@app.post("/loans",response_model=PrestamoResponse)
+@router.post("/loans",response_model=PrestamoResponse)
 def registrar_prestamo():
     pass
 
 # Listar los prestamos
-@app.get("/loans", response_model=List[PrestamoResponse])
+@router.get("/loans", response_model=List[PrestamoResponse])
 def listar_prestamos(skip: int=0, limit: int=10, db:Session=Depends(get_db)):
     prestamos = db.query(Prestamo).offset(skip).limit(limit).all()
     return  prestamos
 
 # Listar prestamo de un libro especifico
-@app.get("/loans/libro{book_id}", response_model=PrestamoResponse)
+@router.get("/loans/libro{book_id}", response_model=PrestamoResponse)
 def obtener_prestamos():
     pass
 
-@app.put("loans/{loan_id}/return")
+@router.put("loans/{loan_id}/return")
 def marcar_prestamo():
     pass
